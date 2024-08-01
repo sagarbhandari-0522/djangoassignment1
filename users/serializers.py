d9d32db4cb940a9578770cbeb5cf7d8d65823078
Sagar Bhandari <sagarvandari1@gmail.com>
2024-08-01 12:55:02 +1200

    fix: remove first-name and last-name from api serializer

--- users/serializers.py
+++ users/serializers.py
@@ -37,4 +37,4 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email']
